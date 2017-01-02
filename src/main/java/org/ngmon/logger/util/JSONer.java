11e9eb769ef22814e3cf4ec21362b9040adf69af
xtovarn <xtovarn@fi.muni.cz>
2017-01-02 17:22:50 +0100

    * payload is serialized

--- src/main/java/org/ngmon/logger/util/JSONer.java
+++ src/main/java/org/ngmon/logger/util/JSONer.java
@@ -10,17 +10,17 @@ import java.util.List;
 import java.util.Map;
 
 public class JSONer {
-    
+
     private static JsonFactory jsonFactory = new JsonFactory();
-    
+
     /**
      * Returns a JSON representation of a particular instance of an event type.
-     * 
-     * @param fqnNS fully qualified name of the @Namespace-annotated class the eventType belongs to
+     *
+     * @param fqnNS     fully qualified name of the @Namespace-annotated class the eventType belongs to
      * @param eventType event type (logging method name)
-     * @param tags a list of tags (e.g. entities) associated with this instance of eventType
-     * @param names names of eventType's attributes
-     * @param values values of eventType's attributes
+     * @param tags      a list of tags (e.g. entities) associated with this instance of eventType
+     * @param names     names of eventType's attributes
+     * @param values    values of eventType's attributes
      * @return JSON object containing all given values
      */
     public static String getEventJson(String fqnNS, String eventType, List<String> tags, String[] names, Object[] values, Level level) {
@@ -51,11 +51,48 @@ public class JSONer {
             json.writeEndObject();
         } catch (IOException e) {
         }
-        
+
         return writer.toString();
     }
 
-    public static String getEventJsonSimple(String type, Map<String,Object> data) {
+    public static String getEventJson2(String fqnNS, String eventType, List<String> tags, String[] names, Object[] values, Level level) {
+        StringWriter writer = new StringWriter();
+        try (JsonGenerator json = jsonFactory.createGenerator(writer)) {
+            json.writeStartObject();
+
+            json.writeArrayFieldStart("tags");
+            for (String t : tags) {
+                json.writeString(t);
+            }
+            json.writeEndArray();
+
+            json.writeStringField("level", level.toString());
+            json.writeStringField("event_namespace", fqnNS);
+            json.writeStringField("event_type", eventType);
+            json.writeStringField("_", getPayload(names, values));
+
+            json.writeEndObject();
+        } catch (IOException e) {
+        }
+
+        return writer.toString();
+    }
+
+    static String getPayload(String[] names, Object[] values) {
+        StringWriter writer = new StringWriter();
+        try (JsonGenerator json = jsonFactory.createGenerator(writer)) {
+            json.writeStartObject();
+            for (int i = 0; i < names.length; i++) {
+                json.writeObjectField(names[i], values[i]);
+            }
+            json.writeEndObject();
+        } catch (IOException e) {
+        }
+
+        return writer.toString();
+    }
+
+    public static String getEventJsonSimple(String type, Map<String, Object> data) {
         StringWriter writer = new StringWriter();
         try (JsonGenerator json = jsonFactory.createGenerator(writer)) {
             json.writeStartObject();
